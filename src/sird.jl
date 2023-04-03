456a7ee6cbd2dc7346f3659b5d156e5997158953
rturquier <remi.turquier@ensae.fr>
2023-04-03 15:17:20 +0200

    🎸 Put SIRD and number of deaths in funktions

--- src/sird.jl
+++ src/sird.jl
@@ -11,14 +11,23 @@ function sird_ode!(du, u, p, t)
     du[4] = d * I
 end
 
-parms = [0.1, 0.05, 0.001]
-init = [0.99, 0.01, 0.0, 0.0]
-tspan = (0.0, 500.0)
-sird_problem = ODEProblem(sird_ode!, init, tspan, parms)
-sird_solution = solve(sird_problem, saveat = 0.1) |> DataFrame
-sird_solution = rename(sird_solution, ["t", "S", "I", "R", "D"])
+function run_sird(;S=0.99, I=0.01, R=0, D=0, i=0.1, r=0.05, d=0.001, end_time=500)
+    initial_conditions=[S, I, R, D]
+    parameters=[i, r, d]
+    time_span=(0.0, end_time)
+    sird_problem = ODEProblem(sird_ode!, initial_conditions, time_span, parameters)
+    sird_solution = solve(sird_problem, saveat = 0.1) |> DataFrame
+    sird_solution = rename(sird_solution, ["t", "S", "I", "R", "D"])
+    return sird_solution
+end
 
-sird_solution |>
+function number_of_deaths(;kwargs...)
+    simulation_result = run_sird(kwargs)
+    number_of_deaths_at_the_end = simulation_result[end, "D"]
+    return number_of_deaths_at_the_end
+end
+
+run_sird() |>
 @vlplot(
     :line,
     transform=[{"fold"=["S", "I", "R", "D"], as=["compartiment", "population"]}],
@@ -26,5 +35,3 @@ sird_solution |>
     y="population:q",
     color="compartiment:n"
 )
-
-sird_solution[end, "D"]

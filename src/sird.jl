22e02a053add6f91b6680b1b14a74ad60d51a932
rturquier <remi.turquier@ensae.fr>
2023-04-03 14:12:06 +0200

    📈 Plot SIRD simulation

--- src/sird.jl
+++ src/sird.jl
@@ -1,4 +1,6 @@
 using DifferentialEquations
+using VegaLite
+using DataFrames
 
 function sird_ode!(du, u, p, t)
     S, I, R, D = u
@@ -11,6 +13,18 @@ end
 
 parms = [0.1, 0.05, 0.001]
 init = [0.99, 0.01, 0.0, 0.0]
-tspan = (0.0, 200.0)
+tspan = (0.0, 500.0)
 sird_problem = ODEProblem(sird_ode!, init, tspan, parms)
-sird_solution = solve(sird_problem, saveat = 0.1)
+sird_solution = solve(sird_problem, saveat = 0.1) |> DataFrame
+sird_solution = rename(sird_solution, ["t", "S", "I", "R", "D"])
+
+sird_solution |>
+@vlplot(
+    :line,
+    transform=[{"fold"=["S", "I", "R", "D"], as=["compartiment", "population"]}],
+    x="t:q",
+    y="population:q",
+    color="compartiment:n"
+)
+
+sird_solution[end, "D"]

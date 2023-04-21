8c3abea4a2d26eab740dd8b7db1455af796b70f1
rturquier <remi.turquier@ensae.fr>
2023-04-21 14:51:04 +0200

    👋 Define the `construct_model` function

--- src/pandprep.jl
+++ src/pandprep.jl
@@ -1,5 +1,17 @@
 module pandprep
 
-greet() = print("Hello World!")
+using Mimi
+
+export construct_model
+
+const model_time = 2023:2523
+
+function construct_model()
+    model = Model()
+
+    set_dimension!(model, :time, model_time)
+
+    return model
+end
 
 end # module pandprep
